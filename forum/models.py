5ed9c8fc44480aed9a5e8f84d7b259a41c929454
Josh Lumb <JoshLumby@hotmail.co.uk>
2022-06-30 14:06:33 +0000

    created model for posts

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Post(models.Model):
+    """  Model for each post that is created """
+    title = models.CharField(max_length=100, unique=True)
+    slug = SlugField(max_length=100, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='author_posts')
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    post_image = CloudinaryField('image', default='placeholder')
+    created_on = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, blank=True, related_name='post_likes')
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    """ Model to comment on posts """
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+
+
+
