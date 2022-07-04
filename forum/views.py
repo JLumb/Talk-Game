c2bfc5bfebba64b0be07dd01d3c2249775fe0838
Josh Lumb <JoshLumby@hotmail.co.uk>
2022-07-04 14:54:23 +0000

    added basic styling for registration and login form

--- forum/views.py
+++ forum/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
+from django.contrib import messages
 from .models import Post, Genre
-from django.contrib.auth.forms import UserCreationForm
 from .forms import RegistrationForm
+from django.contrib.auth import authenticate, login, logout
 
 
 def RegisterPage(request):
@@ -12,6 +13,10 @@ def RegisterPage(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(
+                request, 'Profile successfully created for' + user)
+            return redirect('login')
 
     context = {'form': form}
     return render(request, 'accounts/register.html', context)
